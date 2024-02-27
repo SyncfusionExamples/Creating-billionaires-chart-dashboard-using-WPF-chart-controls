9ade8c5976401e85e20d81d47b9624ed0c9a5820
Sowndharya1911 <Sowndharya.Selladurai@syncfusion.com>
2024-02-27 11:09:29 +0530

    Added sample

---
+++ ChartDashboardDemo/ChartDashboardUI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ChartDashboardUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChartTrackBallBehavior_PositionChanged(object sender, Syncfusion.UI.Xaml.Charts.PositionChangedEventArgs e)
+        {
+            var currentDetails = e.CurrentPointInfos;
+
+            foreach (var item in currentDetails)
+            {
+                var name = item.ValueX;
+
+                var billionaire = viewModel.BillionairesList.FirstOrDefault(x => x.BillionaireName == name);
+                if (billionaire != null)
+                {
+                    rank.Text = billionaire.Rank.ToString();
+                    billionaryName.Text = billionaire.BillionaireName;
+                    netWorth.Text = string.Format("${0}B", billionaire.BillionaireNetWorth);
+                    country.Text = billionaire.BillionaireCountry;
+                }
+            }
+        }
+
+    }
+}
+
---
+++ ChartDashboardDemo/ChartDashboardUI/Model/Model.cs
@@ -0,0 +1,37 @@
+﻿namespace ChartDashboardUI
+{
+    public class Model
+    {
+        public double Rank { get; set; }
+
+        public string BillionaireName { get; set; }
+
+        public double BillionaireNetWorth { get; set; } 
+
+        public string BillionaireCountry { get; set; } 
+        
+        public string Category { get; set; }
+
+        public string Country { get; set; }
+
+        public double CategoryBillionaireCount { get; set; }
+
+        public double CountryBillionaireCount { get; set; }
+
+        public Model(string category, double categoryBillionaireCount, string country, double countryBillionaireCount)
+        {
+            Category = category;
+            CategoryBillionaireCount = categoryBillionaireCount;
+            Country = country;
+            CountryBillionaireCount = countryBillionaireCount;
+        }
+
+        public Model(double rank,string name,double networth, string country)
+        {
+            Rank = rank;
+            BillionaireName = name;
+            BillionaireNetWorth = networth;
+            BillionaireCountry = country;
+        }
+    }
+}
